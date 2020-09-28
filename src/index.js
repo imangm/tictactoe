2e7ba2e07fee1b253c4963ddeff7b04d53c5002f
ImanGM <credo61@gmail.com>
2020-09-28 13:23:31 +0330

    All new styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: "Welcome to Tic Tac Toe",
+			timer: 2000,
+		};
+	}
+	render() {
+		return (
+			<div>
+				<Loader timer={this.state.timer} />
+				<h1>{this.state.title}</h1>
+				<Button />
+			</div>
+		);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Loader extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { loading: true };
+		this.hideLoading = this.hideLoading.bind(this);
+	}
+
+	componentDidMount() {
+		this.hideLoading(this.props.timer);
+	}
+
+	hideLoading(timer) {
+		setTimeout(() => this.setState({ loading: false }), timer);
+	}
+
+	render() {
+		return (
+			<div className={this.state.loading ? "loading loader" : "loaded loader"}>
+				<span>Loading...</span>
+			</div>
+		);
+	}
+}
+
+class Button extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: "Click Me",
+			theClass: "none",
+		};
+		this.handleOnClick = this.handleOnClick.bind(this);
+	}
+
+	handleOnClick() {
+		this.setState({
+			theClass: this.state.theClass === "none" ? "btn" : "none",
+		});
+	}
+
+	componentDidUpdate() {}
+
+	render() {
+		return (
+			<button className={this.state.theClass} onClick={this.handleOnClick}>
+				{this.state.title}
+			</button>
+		);
+	}
+}
+
+ReactDOM.render(<Header />, document.getElementById("root"));
